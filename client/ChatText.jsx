8c2fc39a85351a9525deba52d6e43b586a6c3448
Ian Culleton <ian.culleton.92@gmail.com>
2016-11-25 19:38:54 -0500

    refactored to put chat in front page.

--- client/ChatText.jsx
+++ client/ChatText.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ScrollArea from 'react-scrollbar'
-import MessageList from './MessageList.jsx'
+import Infinite from 'react-infinite'
+import MessageItem from './MessageItem.jsx'
 
 class ChatText extends React.Component {
   constructor(props) {
@@ -19,19 +19,31 @@ class ChatText extends React.Component {
 
   render() {
     return (
-      <ScrollArea
-        speed={0.8}
-        className="area"
-        contentClassName="content"
-        horizontal={false}
-        style={{ height: 200 }}
-        >
-        <MessageList
-          messages={this.state.messages}
-          party={this.props.party}
-          localUser={this.props.localUser}
-        />
-      </ScrollArea>
+      <Infinite
+        containerHeight={200}
+        elementHeight={30}
+        displayBottomUpwards={true}
+      >
+       {
+         this.props.messages.map((message, idx) => {
+           if (message.author === this.props.localUser) {
+             return <MessageItem
+               party={this.props.party}
+               message={message}
+               key={idx}
+            />
+           } else if (message.author) {
+             return <MessageItem
+               party={this.props.party !== "democrat" ? "democrat" : "republican"}
+               message={message}
+               key={idx}
+            />
+           } else {
+             return <div className="chat-text-message-announcement" key={idx}>{message.body}</div>
+           }
+         })
+       }
+      </Infinite>
     )
   }
 
--- client/app.jsx
+++ client/app.jsx
@@ -2,20 +2,83 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router'
+import Infinite from 'react-infinite'
 import Chat from './Chat.jsx'
 import PartyChooser from './PartyChooser.jsx'
-import WaitingRoom from './WaitingRoom.jsx'
 
 // Fron
 
-var App = () => (
-  <div>
-    <Router history={browserHistory}>
-      <Route path="/" component={PartyChooser}></Route>
-      <Route path="/chat/:party/:localUser/:remoteUser" component={Chat}></Route>
-      <Route path="/waiting/:party/:localUser" component={WaitingRoom}></Route>
-    </Router>
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      party: null,
+      localUser: null,
+      remoteUser: null
+    }
+  }
+
+  setUsers(newLocalUser, newRemoteUser, newParty) {
+    this.setState({
+      localUser: newLocalUser,
+      remoteUser: newRemoteUser,
+      party: newParty
+    })
+  }
+  
+  exitChat(e) {
+    if (e) {e.preventDefault();}
+    $.ajax({
+      method: "DELETE",
+      url: "/users/"+this.state.localUser
+    }).then((data) => {
+      console.log(data);
+      this.setState({
+        party: null,
+        localUser: null,
+        remoteUser: null
+      })
+    }).catch((err) => {
+      console.error(err);
+    })
+  }
+
+  scrollBottom() {
+    $("body").animate({scrollTop: $(document).height()-$(window).height()})
+  }
+
+  render() { 
+    return (
+      <div className="app-container">
+        <PartyChooser
+          onConnect={this.setUsers.bind(this)}
+        />
+        { this.state.party && this.state.localUser && this.state.remoteUser ? 
+        <Chat 
+          party={this.state.party}
+          localUser={this.state.localUser}
+          remoteUser={this.state.remoteUser}
+          scrollBottom={this.scrollBottom}
+          exitChat={this.exitChat.bind(this)}
+        /> : 
+        <div></div>
+        }
+        <div className="footer">Some footer text here.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </div>
+    ) 
+  }
+  
+}
+
+ 
+//var App = () => (
+//  <div>
+//    <Router history={browserHistory}>
+//      <Route path="/" component={PartyChooser}></Route>
+//      <Route path="/chat/:party/:localUser/:remoteUser" component={Chat}></Route>
+//      <Route path="/waiting/:party/:localUser" component={WaitingRoom}></Route>
+//    </Router>
+//  </div>
+//)
 
 render(<App />, document.getElementById('app'));
